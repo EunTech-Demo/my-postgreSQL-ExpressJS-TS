9f22fc96a781abef6c1a81e4003d41815b0cf647
chreuseun <eunillevillegaseunille@gmail.com>
2024-12-30 23:30:08 +0800

    create files for handlers of students data

---
+++ src/handlers/students/index.ts
@@ -0,0 +1,5 @@
+import createStudent from "./createStudent";
+import getStudentList from "./getStudentList";
+import updateStudent from "./updateStudent";
+
+export { createStudent, getStudentList, updateStudent };
