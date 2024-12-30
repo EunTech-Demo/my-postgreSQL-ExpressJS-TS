a8190a28cf687b8f07e8fc9975107f7fddc796a3
chreuseun <eunillevillegaseunille@gmail.com>
2024-12-30 19:14:52 +0800

    add search by username wild card in users table

--- src/handlers/users/getAllUsers.ts
+++ src/handlers/users/getAllUsers.ts
@@ -1,8 +1,22 @@
 import { executeSQLQuery } from "@/database/operations";
 import { IUserDTO } from "@/interfaces/users.interface";
+import { generateWildcardSearch } from "@/utils/sql";
 
-const getAllUsers = async () => {
-  const response = await executeSQLQuery("SELECT * FROM crud_system.users");
+const SQL_GET_ALL_USERS = `
+SELECT *
+
+FROM crud_system.users
+
+WHERE 
+  $1::TEXT IS NULL OR $1 = '' OR username LIKE $1::TEXT
+
+ORDER BY created_at DESC
+`;
+
+const getAllUsers = async ({ username }) => {
+  const response = await executeSQLQuery(SQL_GET_ALL_USERS, [
+    generateWildcardSearch(username),
+  ]);
 
   const { rows } = response as {
     rows: IUserDTO[];
