d6e36d4c7494c0cc7169ed332a21ad4013f0cc8c
chreuseun <eunillevillegaseunille@gmail.com>
2025-01-02 16:02:56 +0800

    Update student img

--- src/server.config.ts
+++ src/server.config.ts
@@ -3,7 +3,13 @@ import path from "path";
 const PORT = process?.env?.PORT || 3002;
 const FILE_PATH = path.join(__dirname, "public");
 const STUDENT_IMG_PATH = path.join(__dirname, "public/images/students");
-
 const ALLOWED_IMAGE_TYPES = ["image/jpeg", "image/png", "image/gif"];
+const STUDENT_IMAGE_BASE_URL = `/files/images/students`;
 
-export { PORT, FILE_PATH, STUDENT_IMG_PATH, ALLOWED_IMAGE_TYPES };
+export {
+  PORT,
+  FILE_PATH,
+  STUDENT_IMG_PATH,
+  ALLOWED_IMAGE_TYPES,
+  STUDENT_IMAGE_BASE_URL,
+};
