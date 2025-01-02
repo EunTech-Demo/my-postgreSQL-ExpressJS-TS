ab6256a9fd1b147fe283687ee46b435793f6e8f1
chreuseun <eunillevillegaseunille@gmail.com>
2025-01-02 16:38:01 +0800

    Update student create route

--- src/routers/studentsRoutes/studentsCreate.ts
+++ src/routers/studentsRoutes/studentsCreate.ts
@@ -6,8 +6,43 @@ import { IInsertStudentBody } from "@/interfaces/students.interface";
 import { responseJSONTemplate } from "@/utils/api";
 import { encryptPassword } from "@/utils/auth";
 import { generateFileNameFromRequest } from "@/utils/files";
-import { STUDENT_IMAGE_BASE_URL } from "@/server.config";
+import {
+  ALLOWED_IMAGE_TYPES,
+  STUDENT_IMAGE_BASE_URL,
+  STUDENT_IMG_PATH,
+} from "@/server.config";
+import { fileUploader } from "@/utils/upload";
 
+const insertStudentImgUploadMiddleware = fileUploader(
+  STUDENT_IMG_PATH,
+  ALLOWED_IMAGE_TYPES,
+  "Student Image Not Uploaded",
+  "username"
+).single("image_file");
+
+/**
+ * @api {post} /students/create Create a new student
+ * @apiName CreateStudent
+ * @apiGroup Students
+ *
+ * @apiParam {String} firstname Student's first name
+ * @apiParam {String} lastname Student's last name
+ * @apiParam {String} middlename Student's middle name
+ * @apiParam {String} username Student's username
+ * @apiParam {String} password Student's password
+ * @apiParam {Boolean} is_active Student's active status
+ * @apiParam {File} image_file Student's image (optional)
+ *
+ * @apiSuccess {Boolean} success True if the student was created successfully
+ * @apiSuccess {String} message Success message
+ * @apiSuccess {Object} data An object with the inserted student's ID
+ * @apiSuccess {Number} status HTTP status code
+ *
+ * @apiError {String} error Error message
+ * @apiError {Boolean} success False if the student was not created successfully
+ * @apiError {Object} data Null if the student was not created successfully
+ * @apiError {Number} status HTTP status code
+ */
 const studentsCreate = async (req: Request, res: Response) => {
   const { firstname, lastname, middlename, username, password, is_active } =
     req.body as IInsertStudentBody;
@@ -58,6 +93,4 @@ const studentsCreate = async (req: Request, res: Response) => {
   }
 };
 
-export default studentsCreate;
-
-// create new student
+export default [insertStudentImgUploadMiddleware, studentsCreate];
